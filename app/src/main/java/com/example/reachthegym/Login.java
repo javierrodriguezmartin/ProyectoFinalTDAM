67174b4416278bdcf807cb4c5bbc43fba47d6ac4
Javier Rodriguez Martin <55402708+javierrodriguezmartin@users.noreply.github.com>
2020-03-17 16:15:46 +0100

    First commit

--- app/src/main/java/com/example/reachthegym/Login.java
+++ app/src/main/java/com/example/reachthegym/Login.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Principal extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
